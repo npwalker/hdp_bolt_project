ca80a9d1b7777fd5e815d1e6fcf36d5f54f8d81f
Nick Walker <nick.walker@puppetlabs.com>
2021-08-12 16:36:51 -0500

    initial_commit

---
+++ Puppetfile
@@ -0,0 +1,17 @@
+# This Puppetfile is managed by Bolt. Do not edit.
+# For more information, see https://pup.pt/bolt-modules
+
+# The following directive installs modules to the managed moduledir.
+moduledir '.modules'
+
+mod 'hdp',
+  git: 'https://github.com/npwalker/puppetlabs-hdp',
+  ref: 'fb9b4a4345ef9bbfbfb6b611b775f1b972bc92b3'
+mod 'puppetlabs/docker', '4.1.0'
+mod 'puppetlabs/inifile', '4.1.0'
+mod 'puppetlabs/stdlib', '7.1.0'
+mod 'puppetlabs/apt', '8.1.0'
+mod 'puppetlabs/powershell', '5.0.0'
+mod 'puppetlabs/reboot', '4.0.2'
+mod 'puppetlabs/translate', '2.2.0'
+mod 'puppetlabs/pwshlib', '0.10.0'
---
+++ plans/install.pp
@@ -0,0 +1,31 @@
+# This is the structure of a simple plan. To learn more about writing
+# Puppet plans, see the documentation: http://pup.pt/bolt-puppet-plans
+
+# The summary sets the description of the plan that will appear
+# in 'bolt plan show' output. Bolt uses puppet-strings to parse the
+# summary and parameters from the plan.
+# @summary A plan created with bolt plan new.
+# @param targets The targets to run on.
+plan hdp::install (
+  TargetSpec $targets = "localhost",
+  String     $puppet_primary_hostname,
+  String     $hdp_dns_name = $facts['fqdn']
+) {
+  apply_prep()
+  out::message("Installing docker now")
+  apply( 
+    include 'docker'
+  )
+  out::message("Docker install finished")
+  out::message("Installing HDP now")
+  apply(
+    class { 'hdp::app_stack':
+        ca_server        => "https://${puppet_primary_hostname}:8140",
+        dns_name         => $hdp_dns_name,
+        image_repository => 'gcr.io/hdp-gcp-316600',
+        hdp_version      => 'latest',
+    }
+  )
+  out::message("HDP install finished")
+  return $command_result
+}
